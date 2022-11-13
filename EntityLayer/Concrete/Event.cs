2dc0cd7820aab3f4ca28a890ec467ecb10e63341
berkayonat <berkayonatdemir@gmail.com>
2022-11-13 03:39:15 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public int CategoryId { get; set; }
+        public int CityId { get; set; }
+        public string? Adress { get; set; }
+        public string? Duration { get; set; }
+        public int Capacity { get; set; }
+        public int UserId { get; set; }
+        public bool TicketNeeded { get; set; }
+        public int? Price { get; set; }
+        public bool Status { get; set; } = false;
+        public DateTime EventDate { get; set; }
+        public DateTime ApplicationDeadline { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
